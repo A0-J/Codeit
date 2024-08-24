0a7b3997283a822a4426300616710b886da7be66
oculo0204 <96184457+oculo0204@users.noreply.github.com>
2024-08-25 01:14:40 +0900

    feat: cors 설정 추가

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ import usersRouter from './routes/users.js';
 import groupsRouter from './routes/groups.js';
 import imageRoutes from './routes/images.js';
 import dotenv from 'dotenv';
+import cors from 'cors'; // CORS 패키지 추가
 
 // .env 파일에서 환경 변수 로드
 dotenv.config();
 
 const app = express();
+
+// CORS 설정
+app.use(cors({
+  origin: '*', // 허용할 프론트엔드 URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -27,4 +36,3 @@ app.listen(3000, () => console.log('Server Started on port 3000'));
 mongoose.connect(process.env.DATABASE_URL)
   .then(() => console.log('Connected to DB'))
   .catch(err => console.error('Database connection error:', err));
-
